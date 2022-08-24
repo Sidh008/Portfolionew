a145a495f1d3e144f424f06b8800853325a94a40
Sidh008 <104069933+Sidh008@users.noreply.github.com>
2022-08-24 23:45:57 +0530

    Add files via upload

---
+++ index (1).js
@@ -0,0 +1,37 @@
+$(".home").click(function(){
+    $("section").addClass("disp");
+    $("#home").removeClass("disp");
+  });
+  $(".skills").click(function(){
+      $("section").addClass("disp");
+      $("#Skills").removeClass("disp");
+    });
+    $(".contact").click(function(){
+        $("section").addClass("disp");
+        $("#contact").removeClass("disp");
+      });
+
+    //   $(".home").click(function(){
+    //     $("section").addClass("disp");
+    //     $("#home").removeClass("disp");
+    //   });
+    
+    // $(".skills").click(function(){
+    //     $("section").addClass("disp");
+    //     $("#skills").removeClass("disp");
+    //   });
+    
+    // $(".projects").click(function(){
+    //     $("section").addClass("disp");
+    //     $("#projects").removeClass("disp");
+    //   });
+    
+    // $(".certificates").click(function(){
+    //     $("section").addClass("disp");
+    //     $("#certificates").removeClass("disp");
+    //   });
+    
+    // $(".contact").click(function(){
+    //     $("section").addClass("disp");
+    //     $("#contact").removeClass("disp");
+    //   });
